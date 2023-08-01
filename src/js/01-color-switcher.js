d56b8d29ef1bcfe9efe2f1c69684493ad9db6b09
Alexandr150983 <alexandr150983@gmail.com>
2023-08-01 10:47:22 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let intervalId;
+
+function changeBgdColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    intervalId = setInterval(changeBgdColor, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
+
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+});
+
+
 
